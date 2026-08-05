6b6539da6d6edc7ef97930421558a9b7891effae
ponnadachakravarthi219@gmail.com <ponnadachakravarthi219@gmail.com>
2026-08-05 12:33:22 +0530

    upadate phase 3

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+
+function Profile() {
+  return (
+    <div className="profile-page">
+      <h1>My Profile</h1>
+      <p>Welcome to RideNGo Profile Page</p>
+    </div>
+  );
+}
+
+export default Profile;
