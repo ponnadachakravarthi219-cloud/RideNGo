9a10d5e7b5be21618ce5d3b4ba3c95b9116f6d96
ponnadachakravarthi219@gmail.com <ponnadachakravarthi219@gmail.com>
2026-08-06 20:13:58 +0530

    update phase 4

--- Backend/server.js
+++ Backend/server.js
@@ -1,29 +1,23 @@
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "./server/routes/userRoutes.js";
+import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
-
 app.get("/", (req, res) => {
-  res.send("🚀 RideNGo Backend Running...");
+  res.send("RideNGo Backend Running...");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB Connected");
+const PORT = process.env.PORT || 5000;
 
-    app.listen(process.env.PORT, () => {
-      console.log(`🚀 Server Running on Port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
