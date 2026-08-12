50f39c28ae94c6c98c96c341368c4de434d03524
ponnadachakravarthi219@gmail.com <ponnadachakravarthi219@gmail.com>
2026-08-12 22:39:52 +0530

    update phase 7

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Welcome from "../../components/Welcome/Welcome";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import SummaryCard from "../../components/SummaryCard/SummaryCard";
-import Services from "../../Services/Services";
+// import Services from "../../Services/Services";
 import WhyChoose from "../../WhyChoose/WhyChoose";
 import Fleet from "../../components/Fleet/Fleet";
 import Testimonials from "../../components/Testimonials/Testimonials";
